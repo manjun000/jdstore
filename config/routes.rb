6023124a810ee3f0a064364f966bbcf40db940b8
manjun000 <manj34732@Gmail.com>
2017-01-21 23:45:47 +0800

    change admin/product backend panel

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
+  root "products#index"
   devise_for :users
 
   namespace :admin do
     resources :products
   end
 
-  root "products#index"
+
 
   resources :products
 end
